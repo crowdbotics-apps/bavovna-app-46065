13c6224eed6d3c63973c9cf456e16b0300dfff63
Crowdbotics <team@crowdbotics.com>
2024-02-07 16:15:23 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "bavovna-app-46065",
+  url: "https://bavovna-app-46065.botics.co",
+  api: "https://bavovna-app-46065.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
